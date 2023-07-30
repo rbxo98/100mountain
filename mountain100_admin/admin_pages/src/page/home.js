8438140fa0fe8001ff900915f5a697b19697f173
kye <ye9688@naver.com>
2023-07-30 18:33:59 +0900

    home_testing

--- mountain100_admin/admin_pages/src/page/home.js
+++ mountain100_admin/admin_pages/src/page/home.js
@@ -4,65 +4,26 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Table from 'react-bootstrap/Table';
 import './home.css';
+import { useState } from "react";
 
 function Home() {
+    let [tableValue] = useState([5,'무언가 오류가 있습니다.','김예은','2023.1.19',0]);
 
     return (
       <div>
         <NavbarHead />
         <div className="homepage">
             <Row style={{justifyContent:"space-between"}}>
-                <Col xs={4}>
+                <Col xs={5}>
                     <h3>인증서 발급 현황</h3>
                     <Row className="homepage-row">   
                         <Col>
-                            <img className ="images" src="https://t1.kakaocdn.net/friends/prod/character/character_20221226084804_cb844dd7630a4c9b84f7971f9563b7d1.png" />
-                            <div style={{paddingBottom:"10px"}}>
-                                <h3 className = "checker">미발급: 10</h3>
-                            </div>
-                            <Table bordered>
-                            <thead>
-                                <tr>
-                                    <th style={{backgroundColor: "gainsboro"}}>신청현황</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                </tr>
-                            </tbody>
-                            </Table>
+                            <TitleImages pre = "미발급" value = {10}/>
+                            <TearBodys nowpre = "신청현황"/>
                         </Col>
-                        
                         <Col>
-                            <img className="images" src="https://t1.kakaocdn.net/friends/prod/character/character_20221226084804_cb844dd7630a4c9b84f7971f9563b7d1.png" />
-                            <div style={{paddingBottom:"10px"}}>
-                                <h3 className = "checker">미발급: 10</h3>
-                            </div>
-                            <Table bordered>
-                                <thead>
-                                    <tr>
-                                        <th style={{backgroundColor: "gainsboro"}}>신청현황</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
+                            <TitleImages pre = "발급" value = {10}/>
+                            <TearBodys nowpre = "발급현황"/>
                         </Col>
                     </Row>
                 </Col>
@@ -71,215 +32,89 @@ function Home() {
                     <h3>기념품 처리 현황</h3>
                     <Row className="homepage-row">
                         <Col>
-                            <img className ="images" src="https://t1.kakaocdn.net/friends/prod/character/character_20221226084804_cb844dd7630a4c9b84f7971f9563b7d1.png" />
-                            <div style={{paddingBottom:"10px"}}>
-                                <h3 className = "checker">미발급: 10</h3>
-                            </div>
-                            <Table bordered>
-                            <thead>
-                                <tr>
-                                    <th style={{backgroundColor: "gainsboro"}}>신청현황</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                </tr>
-                            </tbody>
-                            </Table>
+                            <TitleImages pre = "신청" value = {10}/>
+                            <TearBodys nowpre = "신청현황"/>
                         </Col>
 
                         <Col>
-                            <img className="images" src="https://t1.kakaocdn.net/friends/prod/character/character_20221226084804_cb844dd7630a4c9b84f7971f9563b7d1.png" />
-                            <div style={{paddingBottom:"10px"}}>
-                                <h3 className = "checker">미발급: 10</h3>
-                            </div>
-                            <Table bordered>
-                                <thead>
-                                    <tr>
-                                        <th style={{backgroundColor: "gainsboro"}}>신청현황</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
+                            <TitleImages pre = "진행" value = {10}/>
+                            <TearBodys nowpre = "진행현황"/>
                         </Col>
 
                         <Col>
-                            <img className="images" src="https://t1.kakaocdn.net/friends/prod/character/character_20221226084804_cb844dd7630a4c9b84f7971f9563b7d1.png" />
-                            <div style={{paddingBottom:"10px"}}>
-                                <h3 className = "checker">미발급: 10</h3>
-                            </div>
-                            <Table bordered>
-                                <thead>
-                                    <tr>
-                                        <th style={{backgroundColor: "gainsboro"}}>신청현황</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
+                            <TitleImages pre = "완료" value = {10}/>
+                            <TearBodys nowpre = "완료현황"/>
                         </Col>
                     </Row>
                 </Col>
             </Row>
+            
             <Row style={{justifyContent:"space-between"}}>
-                <Col xs={4}>
-                    <h3>인증서 발급 현황</h3>
+                <Col xs={5}>
+                    <h3>Q&A 현황</h3>
                     <Row className="homepage-row">   
                         <Col>
-                            <img className ="images" src="https://t1.kakaocdn.net/friends/prod/character/character_20221226084804_cb844dd7630a4c9b84f7971f9563b7d1.png" />
-                            <div style={{paddingBottom:"10px"}}>
-                                <h3 className = "checker">미발급: 10</h3>
-                            </div>
-                            <Table bordered>
-                            <thead>
-                                <tr>
-                                    <th style={{backgroundColor: "gainsboro"}}>신청현황</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                </tr>
-                            </tbody>
-                            </Table>
+                        <TitleImages pre = "미답변" value = {10}/>
                         </Col>
                         
                         <Col>
-                            <img className="images" src="https://t1.kakaocdn.net/friends/prod/character/character_20221226084804_cb844dd7630a4c9b84f7971f9563b7d1.png" />
-                            <div style={{paddingBottom:"10px"}}>
-                                <h3 className = "checker">미발급: 10</h3>
-                            </div>
-                            <Table bordered>
-                                <thead>
-                                    <tr>
-                                        <th style={{backgroundColor: "gainsboro"}}>신청현황</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
+                            <TitleImages pre = "답변" value = {10}/>
                         </Col>
-                    </Row>
-                </Col>
-                
-                <Col xs={5}>
-                    <h3>기념품 처리 현황</h3>
-                    <Row className="homepage-row">
-                        <Col>
-                            <img className ="images" src="https://t1.kakaocdn.net/friends/prod/character/character_20221226084804_cb844dd7630a4c9b84f7971f9563b7d1.png" />
-                            <div style={{paddingBottom:"10px"}}>
-                                <h3 className = "checker">미발급: 10</h3>
-                            </div>
-                            <Table bordered>
+                        <Table id = "notice-table1">
                             <thead>
-                                <tr>
-                                    <th style={{backgroundColor: "gainsboro"}}>신청현황</th>
+                                <tr >
+                                    <th className="num">번호</th>
+                                    <th className="td-title">제목</th>
+                                    <th className="writer">글쓴이</th>
+                                    <th className="date">작성일</th>
+                                    <th className="counting">조회</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>1</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                </tr>
+                                {tableValue.map((a, i) => {
+                                    return(
+                                        <tr>
+                                            <td >{tableValue[0]}</td>
+                                            <td >{tableValue[1]}</td>
+                                            <td >{tableValue[2]}</td>
+                                            <td >{tableValue[3]}</td>
+                                            <td >{tableValue[4]}</td>
+                                        </tr>
+                                    )
+                                })}
                             </tbody>
-                            </Table>
-                        </Col>
-
-                        <Col>
-                            <img className="images" src="https://t1.kakaocdn.net/friends/prod/character/character_20221226084804_cb844dd7630a4c9b84f7971f9563b7d1.png" />
-                            <div style={{paddingBottom:"10px"}}>
-                                <h3 className = "checker">미발급: 10</h3>
-                            </div>
-                            <Table bordered>
-                                <thead>
-                                    <tr>
-                                        <th style={{backgroundColor: "gainsboro"}}>신청현황</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Col>
-
-                        <Col>
-                            <img className="images" src="https://t1.kakaocdn.net/friends/prod/character/character_20221226084804_cb844dd7630a4c9b84f7971f9563b7d1.png" />
-                            <div style={{paddingBottom:"10px"}}>
-                                <h3 className = "checker">미발급: 10</h3>
-                            </div>
-                            <Table bordered>
-                                <thead>
-                                    <tr>
-                                        <th style={{backgroundColor: "gainsboro"}}>신청현황</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-                        </Col>
+                        </Table>
+                    </Row>
+                    
+                </Col>
+                
+                <Col xs={5}>
+                    <h3>공지사항</h3>
+                    <Row className="homepage-row">
+                        <Table id = "notice-table2">
+                            <thead>
+                                <tr>
+                                    <th className="num">번호</th>
+                                    <th className="td-title">제목</th>
+                                    <th className="writer">글쓴이</th>
+                                    <th className="date">작성일</th>
+                                    <th className="counting">조회</th>
+                                </tr>
+                            </thead>
+                            <tbody >
+                                {tableValue.map((a, i) => {
+                                    return(
+                                        <tr>
+                                            <td >{tableValue[0]}</td>
+                                            <td >{tableValue[1]}</td>
+                                            <td >{tableValue[2]}</td>
+                                            <td >{tableValue[3]}</td>
+                                            <td >{tableValue[4]}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </Table>
                     </Row>
                 </Col>
             </Row>
@@ -287,6 +122,41 @@ function Home() {
         
       </div>
     );
+  }
+
+
+  const TearBodys = (props) => {
+    return (
+        <Table bordered>
+            <thead>
+                <tr>
+                    <th style={{backgroundColor: "gainsboro"}}>{props.nowpre}</th>
+                </tr>
+            </thead>         
+            <tbody>
+                <tr>
+                    <td>브론즈</td>
+                </tr>
+                <tr>
+                    <td>실버</td>
+                </tr>
+                <tr>
+                    <td>골드</td>
+                </tr>
+            </tbody>
+        </Table>
+    )
+  }
+
+  const TitleImages = (props) => {
+    return(
+        <>
+            <img className="images" src="https://t1.kakaocdn.net/friends/prod/character/character_20221226084804_cb844dd7630a4c9b84f7971f9563b7d1.png" />
+            <div style={{paddingBottom:"10px"}}>
+                <h3 className = "checker">{props.pre}: {props.value}</h3>
+            </div>
+        </>
+    )
   }
 
   export default Home;
